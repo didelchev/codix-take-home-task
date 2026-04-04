59a5c27c4b62a77eb95ea52ab1e3c0f659f637fd
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-04 15:00:13 +0300

    add initial folder structure

---
+++ src/app/components/transaction-modal/transaction-modal.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-modal',
+  imports: [],
+  templateUrl: './transaction-modal.html',
+  styleUrl: './transaction-modal.css',
+})
+export class TransactionModal {}
