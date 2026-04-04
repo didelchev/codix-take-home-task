12feaf77e06fc0272e3aefdd620ede1360b22651
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-04 21:10:34 +0300

    add application routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { ClientProfile } from './components/client-profile/client-profile';
+import { Transactions } from './components/transactions/transactions';
+import { NotFound } from './components/not-found/not-found';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'client',
+        pathMatch: 'full'
+
+    },
+
+    {
+        path: 'client',
+        component: ClientProfile
+    },
+    {
+        path: 'transactions',
+        component: Transactions
+    },
+    {
+        path: "**",
+        component: NotFound
+    }
+];
