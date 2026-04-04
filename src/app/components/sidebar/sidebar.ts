492459b5ddbe83cb2af4348b4f06444a73ea5c50
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-04 23:55:37 +0300

    add sidebar navigation and dynamic clock

--- src/app/components/sidebar/sidebar.ts
+++ src/app/components/sidebar/sidebar.ts
@@ -1,9 +1,50 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit, signal } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { LucideAngularModule, User, DollarSign, Palette } from 'lucide-angular';
+
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive, LucideAngularModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.css',
 })
-export class Sidebar {}
+export class Sidebar implements OnInit, OnDestroy {
+
+  time = signal('');
+  period = signal('');
+  day = signal('');
+  date = signal('');
+
+
+  readonly UserIcon = User
+  readonly DollarIcon = DollarSign 
+  readonly PaletteIcon = Palette
+  
+  private intervalId: any;
+
+  ngOnInit(): void {
+    this.updateClock();
+    this.intervalId = setInterval(() => this.updateClock(), 1000);
+  }
+
+  ngOnDestroy(): void {
+    clearInterval(this.intervalId);
+  }
+  
+  private updateClock(): void{
+    const now = new Date();
+
+    const hours = now.getHours() % 12 || 12;
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0')
+    this.time.set(`${hours}:${minutes}:${seconds}`);
+    this.period.set(now.getHours() >= 12 ? 'PM' : 'AM');
+
+    this.day.set(now.toLocaleDateString('en-US', { weekday: 'long' }));
+    this.date.set(now.toLocaleDateString('en-US', { month: 'long', day: 'numeric', year: 'numeric' }));
+
+  }
+
+
+}
