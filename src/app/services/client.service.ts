7002632e8c2d2ced75b80c473a9d2404c63bcae8
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-05 14:48:58 +0300

    add client service

--- src/app/services/client.service.ts
+++ src/app/services/client.service.ts
@@ -1,6 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
+import { Client } from '../models/client.model';
+
 
 @Injectable({
   providedIn: 'root',
 })
-export class ClientService {}
+export class ClientService {
+  private CLIENT_URL = 'assets/client.json';
+  private TRANSACTIONS_URL = 'assets/client.json'
+
+  constructor(private http: HttpClient){};
+
+  getClient(): Observable<Client>{
+    return this.http.get<Client>(this.CLIENT_URL).pipe(
+      catchError(() => throwError(() => "Failed to load client data !"))
+    )
+  }
+
+
+  getTransactions(): Observable<Transaction[]>{
+    return this.http.get<Transaction[]>(this.TRANSACTIONS_URL).pipe(
+      catchError(() => throwError(() => "Failed to load transactions data !"))
+    )
+  }
+}
