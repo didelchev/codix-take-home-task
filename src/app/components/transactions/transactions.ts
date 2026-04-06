9f0a0805701fe01cc14ba44735b5bfcec53e3a53
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-06 14:05:21 +0300

    add transactions view

--- src/app/components/transactions/transactions.ts
+++ src/app/components/transactions/transactions.ts
@@ -1,9 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-transactions',
-  imports: [],
+  imports: [NgClass],
   templateUrl: './transactions.html',
   styleUrl: './transactions.css',
 })
-export class Transactions {}
+export class Transactions implements OnInit{
+
+  transactions: Transaction[]  | null = [];
+  isReady = false;
+  errorMessage = ''
+
+  constructor(private transactionService: ClientService){}
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.transactionService.getTransactions().subscribe({
+        next: ( data) => {
+          this.transactions = data;
+          this.isReady = true;
+
+        },
+        error: (err) => {
+          this.errorMessage = err;
+          this.isReady = true
+        }
+      })
+    }, 1000)
+  }
+
+
+}
