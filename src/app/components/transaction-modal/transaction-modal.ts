0375b935b6e119423c53b84eebc39aa3dd18cb52
Daniel Delchev <daniel.delchev@yahoo.com>
2026-04-06 21:26:47 +0300

    add transaction modal

--- src/app/components/transaction-modal/transaction-modal.ts
+++ src/app/components/transaction-modal/transaction-modal.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CustomCurrencyPipe } from '../../pipes/currency.pipe';
+import { CustomDatePipe } from '../../pipes/date.pipe';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-transaction-modal',
-  imports: [],
+  imports: [CustomCurrencyPipe, CustomDatePipe, NgClass],
   templateUrl: './transaction-modal.html',
   styleUrl: './transaction-modal.css',
 })
-export class TransactionModal {}
+export class TransactionModal {
+  @Input() transaction: Transaction | null = null;
+  @Output() close = new EventEmitter<void>();
+
+  onClose(): void { 
+    this.close.emit();
+  }
+}
